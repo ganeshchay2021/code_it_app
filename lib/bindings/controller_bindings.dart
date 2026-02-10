ed47a084e2a694f5b9d00f93e10e55c21435033c
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-10 22:07:09 +0545

    Feat(Binding): Controller Bindings Updated

--- lib/bindings/controller_bindings.dart
+++ lib/bindings/controller_bindings.dart
@@ -1,3 +1,4 @@
+import 'package:counter_app/controller/popular_course_controller.dart';
 import 'package:counter_app/controller/upcomming_class_controller.dart';
 import 'package:get/get.dart';
 
@@ -5,5 +6,7 @@ class ControllerBindings extends Bindings{
   @override
   void dependencies() {
     Get.put<UpcommingClassController>(UpcommingClassController(), permanent: true);
+    Get.put<PopularCourseController>(PopularCourseController(), permanent: true);
+
   }
 }
