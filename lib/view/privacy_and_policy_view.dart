c0ccbd40d9d9cf329dbf9b82e99b1d8dca5bb71c
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-12 21:18:57 +0545

    Feat(View): Privacy and Policy View Added

---
+++ lib/view/privacy_and_policy_view.dart
@@ -0,0 +1,53 @@
+import 'package:counter_app/controller/pricavy_and_policy_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:get/get.dart';
+
+class PrivacyAndPolicyView extends GetView<PricavyAndPolicyController> {
+  const PrivacyAndPolicyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Privacy And Policy"),
+      ),
+      body: Obx(() {
+        if (controller.isLoading.isTrue) {
+          return Center(child: CircularProgressIndicator.adaptive());
+        }else{
+          return ListView(
+            children: [
+                Html(
+                data:
+                    """
+       ${controller.privacyAndPolicy.value.data!.description}
+        """,
+                extensions: [
+                  TagExtension(
+                    tagsToExtend: {"flutter"},
+                    child: const FlutterLogo(),
+                  ),
+                ],
+                style: {
+                  "p.fancy": Style(
+                    fontSize: FontSize(30),
+                    textAlign: TextAlign.center,
+                    backgroundColor: Colors.grey,
+                    margin: Margins(
+                      left: Margin(50, Unit.px),
+                      right: Margin.auto(),
+                    ),
+                    width: Width(300, Unit.px),
+                    fontWeight: FontWeight.bold,
+                  ),
+                },
+              ),
+            ],
+          );
+        }
+      }),
+    );
+  }
+}
