6a1761600c8ae7bb34dd31b5342879f57cbc1c22
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-02-12 18:41:31 +0545

    Feat(View): Terms and Conditions View Added

---
+++ lib/view/terms_and_condition_view.dart
@@ -0,0 +1,50 @@
+import 'package:counter_app/controller/terms_and_condition_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:get/get.dart';
+
+class TermsAndConditionView extends GetView<TermsAndConditionController> {
+  const TermsAndConditionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Terms and Condition")),
+      body: Obx(() {
+        if (controller.isLoading.isTrue) {
+          return Center(child: CircularProgressIndicator.adaptive());
+        } else {
+          return ListView(
+            children: [
+              Html(
+                data:
+                    """
+       ${controller.termsAndCondition.value.data!.description}
+        """,
+                extensions: [
+                  TagExtension(
+                    tagsToExtend: {"flutter"},
+                    child: const FlutterLogo(),
+                  ),
+                ],
+                style: {
+                  "p.fancy": Style(
+                    fontSize: FontSize(30),
+                    textAlign: TextAlign.center,
+                    backgroundColor: Colors.grey,
+                    margin: Margins(
+                      left: Margin(50, Unit.px),
+                      right: Margin.auto(),
+                    ),
+                    width: Width(300, Unit.px),
+                    fontWeight: FontWeight.bold,
+                  ),
+                },
+              ),
+            ],
+          );
+        }
+      }),
+    );
+  }
+}
